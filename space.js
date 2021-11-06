88a8ab4767e20b16bac28f215785c219f76fe628
Filip Johansson <filip.johansson@protonmail.com>
2021-11-06 21:01:13 +0100

    completely funcitonal!!!

--- space.js
+++ space.js
@@ -7,9 +7,15 @@ const context = canvas.getContext("2d");
 context.fillStyle = "black";
 context.fillRect(0, 0, canvas.width, canvas.height);
 
+window.onresize = () => {
+  canvas.width = canvas.offsetWidth;
+  canvas.height = canvas.offsetHeight;
+};
+
 // Constants
 const warpZ = 12;
 const speed = 0.075;
+const velocity = { x: 0, y: 0, z: -speed };
 
 // Utils
 
@@ -17,19 +23,29 @@ const half = (x) => x / 2;
 const range = (n) => [...Array(n).keys()];
 const centerOf = ({ width, height }) => [half(width), half(height)];
 const random = (x, y) => Math.floor(Math.random() * y * 2) + x;
-const calculateVelocity = () => vec3.fromValues(0, 0, -speed);
 const getColor = () => "hsla(200,100%, " + random(50, 100) + "%, 1)";
+
 const outside = ({ x, y }) =>
   x < -half(canvas.width) ||
   x > half(canvas.width) ||
   y < -half(canvas.height) ||
   y > half(canvas.height);
-const newStarVector = () =>
-  vec3.fromValues(
-    random(-half(canvas.width), half(canvas.width)),
-    random(-half(canvas.height), half(canvas.height)),
-    random(1, warpZ)
-  );
+
+const addVector = (v1, v2) => ({
+  x: v1.x + v2.x,
+  y: v1.y + v2.y,
+  z: v1.z + v2.z,
+});
+
+// Star
+
+const newStarVector = () => ({
+  x: random(-half(canvas.width), half(canvas.width)),
+  y: random(-half(canvas.height), half(canvas.height)),
+  z: random(1, warpZ),
+});
+
+const starToDrawable = (star) => ({ ...star, ...star.dv });
 
 const drawStar = ({ x, y, dx, dy, color }) => {
   context.strokeStyle = color;
@@ -39,14 +55,9 @@ const drawStar = ({ x, y, dx, dy, color }) => {
   context.stroke();
 };
 
-const starObjectToMap = (star) => ({
-  ...star.dv,
-  color: star.color,
-});
-
 const getNextStarVector = ({ v, velocity, speed }) => {
-  const nextVector = vec3.add(vec3.create(), v, velocity);
-  const [x2, y2, z2] = nextVector;
+  const nextVector = addVector(v, velocity);
+  const { x: x2, y: y2, z: z2 } = nextVector;
 
   return {
     x: x2 / z2,
@@ -58,63 +69,56 @@ const getNextStarVector = ({ v, velocity, speed }) => {
 };
 
 const resetStar = (star) => {
-  const { x, y, nextVector } = star.dv;
+  const {
+    dv: { x, y, nextVector },
+  } = star;
 
   if (outside({ x, y })) {
-    star.v = newStarVector();
-    star.x = star.v[0];
-    star.y = star.v[1];
+    return { ...star, v: newStarVector() };
   } else {
-    star.v = nextVector;
+    return { ...star, v: nextVector };
   }
 };
 
 const updateStar = (star) => {
-  star.dv = getNextStarVector({
-    v: star.v,
-    velocity: star.velocity,
-    speed: star.speed,
-  });
+  // star.dv = getNextStarVector(star);
+  return { ...star, dv: getNextStarVector(star) };
 };
 
-const Star = function ({ color, velocity, speed }) {
-  this.color = color;
-  this.v = newStarVector();
-  this.dv = getNextStarVector({ v: this.v, velocity, speed });
-  this.x = this.v[0];
-  this.y = this.v[1];
-  this.z = this.v[2];
-  this.velocity = velocity;
-  this.speed = speed;
+const Star = ({ color, velocity, speed, v }) => {
+  const { x, y, z } = v;
+
+  return {
+    velocity,
+    color,
+    speed,
+    x,
+    y,
+    z,
+    v,
+  };
 };
 
 const render = (stars) => () => {
   context.setTransform(1, 0, 0, 1, 0, 0);
   context.fillStyle = "rgba(0,0,0,0.2)";
   context.fillRect(0, 0, canvas.width, canvas.height);
-
   context.translate(...centerOf(canvas));
-  stars.forEach(updateStar);
 
-  stars.map(starObjectToMap).forEach(drawStar);
+  const nextStars = stars.map(updateStar).map(resetStar);
 
-  stars.forEach(resetStar);
+  nextStars.map(starToDrawable).forEach(drawStar);
 
-  requestAnimationFrame(render(stars));
+  requestAnimationFrame(render(nextStars));
 };
 
-const stars = range(250).map(
-  (_) =>
-    new Star({
-      color: getColor(),
-      velocity: calculateVelocity(),
-      speed,
-    })
+const stars = range(250).map((_) =>
+  Star({
+    velocity,
+    speed,
+    color: getColor(),
+    v: newStarVector(),
+  })
 );
 
 render(stars)();
-
-window.onresize = function () {
-  canvas.width = canvas.offsetWidth;
-  canvas.height = canvas.offsetHeight;
-};
